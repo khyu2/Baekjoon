94c05d09191ea079da935708f7e3bbb7f23c802a
Hyunn <66515313+lk-Hyun@users.noreply.github.com>
2024-03-03 02:56:36 +0900

    [Gold IV] Title: 타임머신, Time: 348 ms, Memory: 36220 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -1,31 +1,45 @@
 import sys
 import math
+from collections import deque
 input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split())
 a = [[] for _ in range(n + 1)]
-distance = [math.inf] * (n + 1)
 
 for _ in range(m):
-  u, v, w = map(int, input().rstrip().split())
-  a[u-1].append([v-1, w])
+  x, y, z = map(int, input().rstrip().split())
+  a[x].append([y, z])
 
-def bellman_ford(s): # return True if minusCycle exists
-  distance[s] = 0
+def spfa(start):
+  dist = [math.inf] * (n + 1)
+  vis = [0] * (n + 1)
+  inQ = [False] * (n + 1)
 
-  for i in range(n):
-    for j in range(n):
-      for nxt, dst in a[j]: # u -> v : w
-        if distance[j] != math.inf and distance[nxt] > distance[j] + dst:
-          distance[nxt] = distance[j] + dst
-          if i == n - 1: return True
+  q = deque([start])
+  dist[start] = 0
+  inQ[start] = True
+  vis[start] += 1
 
-  return False
+  while q:
+    cur = q.popleft()
+    inQ[cur] = False
 
-minus_cycle = bellman_ford(0)
+    for v, w in a[cur]:
+      if dist[v] > dist[cur] + w:
+        dist[v] = dist[cur] + w
 
-if minus_cycle:
-  print(-1)
+        if not inQ[v]:
+          q.append(v)
+          inQ[v] = True
+          vis[v] += 1
+          if vis[v] >= n:
+            return []
+          
+  return dist[2:]
+
+dist = spfa(1)
+if dist:
+  for i in dist:
+    print(i if i != math.inf else -1)
 else:
-  for i in range(1, n):
-    print(distance[i] if distance[i] != math.inf else -1)
+  print(-1)
