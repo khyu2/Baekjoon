343f6ce7381c5c401b9f3a1f42afd8bc19baf1b0
Hyunn <66515313+lk-Hyun@users.noreply.github.com>
2024-03-03 23:46:13 +0900

    [Silver III] Title: N과 M (1), Time: 116 ms, Memory: 35556 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,9 @@
 import sys
+from itertools import permutations
 input = sys.stdin.readline
 
 n, m = map(int, input().rstrip().split())
-a = []
-vis = [False] * (n + 1)
+a = list(range(1, n + 1))
 
-def dfs(depth):
-  if depth == m:
-    print(*a)
-    return
-  
-  for i in range(1, n + 1):
-    if not vis[i]:
-      vis[i] = True
-      a.append(i)
-      dfs(depth + 1)
-      a.pop()
-      vis[i] = False
-
-dfs(0)
+for i in list(permutations(a, m)):
+  print(' '.join(map(str, i)))
