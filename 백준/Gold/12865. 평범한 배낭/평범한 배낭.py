17568f5084d9c4320b1745d6959226f22040884f
Hyunn <66515313+lk-Hyun@users.noreply.github.com>
2024-03-07 00:39:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 396 ms, Memory: 189720 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,11 +2,16 @@ import sys
 input = lambda: sys.stdin.readline().rstrip()
 
 n, k = map(int, input().split())
-w, v = zip(*(map(int, input().split()) for _ in range(n)))
-d = [0] * (k + 1)
+d = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
+w, v = zip(*[map(int, input().split()) for _ in range(n)])
+w = [0] + list(w)
+v = [0] + list(v)
 
-for i in range(n):
-  for j in range(k, 0, -1):
-    if w[i] <= j: d[j] = max(d[j], d[j - w[i]] + v[i])
+for i in range(1, n + 1):
+  for j in range(1, k + 1):
+    if j >= w[i]:
+      d[i][j] = max(d[i-1][j], d[i-1][j - w[i]] + v[i])
+    else:
+      d[i][j] = d[i-1][j]
 
-print(d[k])
+print(d[n][k])
