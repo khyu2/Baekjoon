516bfd443f49eb122b71f808ae53dfdda3695da5
Hyunn <66515313+lk-Hyun@users.noreply.github.com>
2024-03-10 01:24:44 +0900

    [Gold II] Title: 피보나치 수 6, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -1,49 +1,49 @@
 import sys
-input = sys.stdin.readline
+input = lambda: sys.stdin.readline().rstrip()
 
-MOD = 10 ** 9 + 7
+MOD = 10**9 + 7
 
 class Matrix:
-    def __init__(self, data):
-        self.data = data
+  def __init__(self, data):
+    self.data = data
+    self.MOD = 10**9 + 7
+  
+  def __mul__(self, other):
+    n = len(self.data)
 
-    def __mul__(self, other):
-        n = len(self.data)
-        
-        result = [[0] * n for _ in range(n)]
-        for i in range(n):
-            for j in range(n):
-                for k in range(n):
-                    result[i][j] += self.data[i][k] * other.data[k][j]
-                    result[i][j] %= MOD
-        
-        return Matrix(result)
+    res = [[0] * n for _ in range(n)]
+    for i in range(n):
+      for j in range(n):
+        for k in range(n):
+          res[i][j] += self.data[i][k] * other.data[k][j]
+          res[i][j] %= self.MOD
 
-    def __mod__(self, other):
-        n = len(self.data)
+    return Matrix(res)
+  
+  def __mod__(self, MOD):
+    n = len(self.data)
 
-        for i in range(n):
-            for j in range(n):
-                self.data[i][j] %= other
+    for i in range(n):
+      for j in range(n):
+        self.data[i][j] %= MOD
 
-        return Matrix(self.data)
+    return Matrix(self.data)
 
-
-    def __str__(self):
-        return '\n'.join([' '.join(map(str, row)) for row in self.data])
-    
-    def get_fibo(self):
-        return self.data[0][1]
+  def __str__(self):
+    return '\n'.join([' '.join(map(str, row)) for row in self.data])
+  
+  def get_fibo(self):
+    return self.data[0][1]
 
 def power(x, n):
-    if n == 0: return Matrix([1, 0], [0, 1])
-    if n == 1: return x % MOD
-    half = power(x, n // 2)
-
-    if n & 1: return half * half * x
-    else: return half * half
+  if n == 0: return Matrix([[1, 0], [0, 1]])
+  if n == 1: return x % MOD
+  half = power(x, n // 2)
+  if n & 1: return half * half * a
+  else: return half * half
 
 a = Matrix([[1, 1], [1, 0]])
 
-n = int(input().rstrip())
+n = int(input())
+
 print(power(a, n).get_fibo())
