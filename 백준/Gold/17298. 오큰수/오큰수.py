9cf027dda791f594e7dddc86d5116b8b3c6b7dab
Hyunn <66515313+lk-Hyun@users.noreply.github.com>
2024-03-11 22:58:46 +0900

    [Gold IV] Title: 오큰수, Time: 448 ms, Memory: 249260 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -1,23 +1,18 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+def next_greater_element(nums):
+    stack = []
+    result = [-1] * len(nums) # 결과를 저장할 배열을 -1로 초기화
+    
+    for i in range(len(nums)):
+        while stack and nums[stack[-1]] < nums[i]:
+            result[stack.pop()] = nums[i] # 현재 값이 스택의 top에 해당하는 인덱스의 다음 큰 수임을 기록
+        stack.append(i) # 현재 인덱스를 스택에 추가
+    
+    return result
+
 n = int(input())
 a = list(map(int, input().split()))
-stack = []
-ans = [-1] * n
 
-for i, x in enumerate(a):
-  if not stack:
-    stack.append([x, i])
-  elif stack and x < stack[-1][0]:
-    stack.append([x, i])
-  else:
-    while stack and x > stack[-1][0]:
-      ans[stack[-1][1]] = x
-      stack.pop()
-    
-    stack.append([x, i])
-  # print('stack = ', stack)
-  # print('answer = ', ans)
-
-print(' '.join(map(str, ans)))
+print(' '.join(map(str, next_greater_element(a))))
