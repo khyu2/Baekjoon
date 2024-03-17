f542e1b7bcb95ac14bfd476542534853b314a0ff
Hyunn <66515313+lk-Hyun@users.noreply.github.com>
2024-03-18 02:16:59 +0900

    [Gold V] Title: 집합의 표현, Time: 412 ms, Memory: 125080 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -4,12 +4,12 @@ input = lambda: sys.stdin.readline().rstrip()
 class UnionFind:
   def __init__(self, n):
     self.p = [-1] * n
-    
+
   def find(self, n):
     if self.p[n] < 0: return n
     self.p[n] = self.find(self.p[n])
     return self.p[n]
-  
+
   def union(self, a, b):
     a = self.find(a)
     b = self.find(b)
@@ -17,14 +17,18 @@ class UnionFind:
     self.p[a] += self.p[b]
     self.p[b] = a
 
+  def getSize(self, n):
+    return abs(self.p[n])
+  
+  def same_set(self, a, b):
+    return self.find(a) == self.find(b)
+
 n, m = map(int, input().split())
 uf = UnionFind(n + 1)
 
 for _ in range(m):
   cmd, a, b = map(int, input().split())
   if cmd:
-    a = uf.find(a)
-    b = uf.find(b)
-    print('YES' if a == b else 'NO')
+    print('YES' if uf.same_set(a, b) else 'NO')
   else:
     uf.union(a, b)
